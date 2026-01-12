18f7c8234e6d02f5781824f0193301c4e59a7045
Teshome <teshomebf@gmail.com>
2026-01-13 00:24:26 +0300

    Add certification section with slider, right/left arrow functionality, and wrap-around logic

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+
+/*
+===============
+    HEADER
+===============*/ 
+
+//Mobile menu
 const hamburger = document.getElementById('btn');
 const navBar = document.getElementById('navbar');
 
 const navLinks = document.querySelectorAll('.navbar a');
 
 const textMenu = document.querySelector('.menu-text');
+
+const mainContent = document.querySelector('main');
 
 hamburger.addEventListener(
     'click', () => {
@@ -21,4 +30,43 @@ navLinks.forEach(link => {
     });
 });
 
+/*
+=====================
+CERTIFICATION SECTION
+=====================*/
 
+//carousel for certificates
+const slideContainer = document.querySelector(".slides");
+const slides = document.querySelectorAll(".slides img");
+
+let slideCounter = 0;
+
+
+// Right arrow logic for carousel
+const rightArrow = document.querySelector(".arrow.right");
+
+rightArrow.addEventListener('click', () => {
+    slideCounter++;
+
+    if(slideCounter > slides.length - 1) {
+        slideCounter = 0;
+    }
+
+    slideContainer.style.transform = `translateX(-${slideCounter * 100}%)`;
+
+});
+
+// Left arrow logic for carousel
+
+const leftArrow = document.querySelector(".arrow.left");
+
+leftArrow.addEventListener('click', () => {
+    slideCounter--;
+
+    if(slideCounter < 0) {
+        slideCounter = slides.length - 1;
+    }
+
+slideContainer.style.transform = `translateX( -${slideCounter * 100}%)`;
+console.log(slideCounter);
+})
